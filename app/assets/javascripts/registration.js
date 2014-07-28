f95f5c73e98d7920eff60bd57470e14c3e89997f
David Hsu <dvdhsu@gmail.com>
2014-07-28 04:43:29 +0000

    Autocomplete college and names on user.

---
+++ app/assets/javascripts/registration.js
@@ -0,0 +1,17 @@
+$(document).ready(function() { $("#collegeSelect").select2(); });
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+jQuery(function($){
+  $('#emailField').change(function() {
+    var collegePatt = /@(.*).ox.ac.uk/;
+    var email = $('#emailField').val();
+    var names = email.split("@")[0].split(".");
+
+    $('#collegeSelect').select2('val', email.match(collegePatt)[1]);
+    $('#firstNameField').val(names[0].capitalize());
+    $('#lastNameField').val(names[1].capitalize());
+  });
+});
