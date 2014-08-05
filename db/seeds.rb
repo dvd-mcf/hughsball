239a43c524360d650ddb6156968e79a37ead9d5f
David Hsu <dvdhsu@gmail.com>
2014-08-05 01:34:45 -0700

    Change database seeding to conform to new user model. Remove seeding from production.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,38 @@
-# Generated with RailsBricks
 # Initial seed file to use with Devise User Model
 
 # Temporary admin account
-u = User.new(
-    username: "admin",
-    email: "admin@example.com",
-    password: "1234",
-    password_confirmation: "1234",
-    admin: true
-)
-u.skip_confirmation!
-u.save!
-
-
-
-# Test user accounts
-(1..50).each do |i|
+case Rails.env
+unless 'production'
   u = User.new(
-      username: "user#{i}",
-      email: "user#{i}@example.com",
+      email: "admin@example.com",
       password: "1234",
-      password_confirmation: "1234"
+      password_confirmation: "1234",
+      admin: true,
+      first_name: "Admin",
+      last_name: "Admin",
+      college: "ccc"
   )
   u.skip_confirmation!
   u.save!
-
-  puts "#{i} test users created..." if (i % 5 == 0)
-
-end
   
-
+  
+  
+  # Test user accounts
+  (1..50).each do |i|
+    u = User.new(
+        email: "user#{i}@example.com",
+        password: "1234",
+        password_confirmation: "1234",
+        first_name: "first_name#{i}",
+        last_name: "last_name#{i}",
+        college: "ccc"
+    )
+    u.skip_confirmation!
+    u.save!
+  
+    puts "#{i} test users created..." if (i % 5 == 0)
+  
+  end
+end
+    
+  
