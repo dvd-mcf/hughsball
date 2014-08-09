b6d0dedb13f072ba94b95303ed74a45d16d2add5
David Hsu <dvdhsu@gmail.com>
2014-08-09 07:12:00 +0000

    add conditional validation so admin users don't need an Oxford email

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
   @@college_regex = /\A(.+)@(.+)\.ox\.ac\.uk\Z/  
 
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-  validates_format_of :email, with: @@college_regex,  message: "must be from Oxford"
+  validates_format_of :email, with: @@college_regex,  message: "must be from Oxford", unless: "admin?"
   validates :first_name, :last_name, :college, presence: true
   validate :hughs_only_if_email
   
