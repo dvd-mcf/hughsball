f8ab33c3333969a9b54ab951a18527fb40037ce8
David Hsu <dvdhsu@gmail.com>
2014-08-11 16:23:06 -0700

    improve home page layout on mobile

---
+++ app/assets/javascripts/mobileNavbar.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  under768();
+  $(window).resize(function() {
+    under768();
+    if ($(window).width() > 768) {
+      $('.navbar').addClass('navbar-fixed-top');
+      $('body').css('padding-top', '50px');
+    }
+  });
+
+  function under768() {
+    if ($(window).width() < 768) {
+      $('.navbar').removeClass('navbar-fixed-top');
+      $('body').css('padding-top', '0px');
+      $('.navbar').css('margin-bottom', '0px');
+    }
+  }
+});
+
