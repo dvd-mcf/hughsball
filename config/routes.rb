810be22c2a70efbafda8d3430464405b54d13ed1
David Hsu <dvdhsu@gmail.com>
2014-08-17 17:13:02 -0700

    remove charges pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Railstest::Application.routes.draw do
   root "pages#home"    
   get "home", to: "pages#home", as: "home"
   resources :tickets
-  resources :charges
   
     
   devise_for :users, :controllers => { registrations: 'registrations' }
