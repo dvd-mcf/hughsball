049f64f7a28dec142da921f4b385a286b634f98a
David Hsu <dvdhsu@gmail.com>
2014-08-22 06:22:45 +0000

    regenerate schema and remove seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 20140822055315) do
     t.string   "encrypted_password",     default: "",    null: false
     t.boolean  "admin",                  default: false, null: false
     t.boolean  "locked",                 default: false, null: false
-    t.string   "slug"
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -53,6 +52,5 @@ ActiveRecord::Schema.define(version: 20140822055315) do
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["slug"], name: "index_users_on_slug", unique: true
 
 end
