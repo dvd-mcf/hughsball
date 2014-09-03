87713efc9ad582db5d0ba0d008b2535b87915b32
David Hsu <dvdhsu@gmail.com>
2014-09-03 02:01:23 -0700

    Update assets.rb to fix locale issues.
    Effectively rolls back the last commit. Instead manually adds in
    filenames for asset precompilation.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,16 +1,2 @@
-# config/initializers/assets.rb
-Rails.application.config.assets.precompile << Proc.new do |path|
-  if path =~ /\.(js)\z/
-    full_path = Rails.application.assets.resolve(path).to_path
-    app_assets_path = Rails.root.join('app', 'assets').to_path
-    if full_path.starts_with? app_assets_path
-      puts "including asset: " + full_path
-      true
-    else
-      puts "excluding asset: " + full_path
-      false
-    end
-  else
-    false
-  end
-end
+Rails.application.config.assets.precompile += ['registration.js', 'scrollspy_refresh.js', 
+  'ticket.js']
