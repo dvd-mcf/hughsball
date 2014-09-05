59aa117db30a3488a33b311d5488147b64b0caf7
David Hsu <dvdhsu@gmail.com>
2014-09-04 20:08:38 -0700

    Add "add user" function to admin page.
    Admins can now add users. I've added both `new' and `create' actions to
    the admin user controller, as well as added a view, which is basically
    the same as the Devise `new registration' view. Routes were unchanged,
    since there was already a path for `new_admin_user', as well as one
    handling a post to `admin_users'.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -17,6 +17,23 @@ class Admin::UsersController < Admin::BaseController
   
   def edit
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.skip_confirmation!
+      @user.save
+      redirect_to admin_users_path, notice: "#{@user.email} created."
+    else
+      @errors = @user.errors
+      flash.now[:error] = @errors.full_messages[0]
+      render 'new'
+    end
+  end
   
   def update
     new_params = user_params.dup
