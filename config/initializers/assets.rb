353bef053ebccdb534bdd2db3c7df13038041d51
David Hsu <dvdhsu@gmail.com>
2014-09-12 22:42:00 -0800

    Add `home.css' and `ticket.css' to asset compilation.
    Since the assets aren't included in `application.css.scss', we've got to
    compile them manually, by adding them to the asset compilation
    initializer.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,4 @@
 Rails.application.config.assets.precompile += ['registration.js', 'scrollspy_refresh.js', 
   'ticket.js']
+
+Rails.application.config.assets.precompile += ['home.css', 'ticket.css']
